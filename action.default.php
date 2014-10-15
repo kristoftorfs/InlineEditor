bdbf3c1a3f32f501c00bcc149f048bbb75f6bbf5
Kristof Torfs <kristof.torfs@econoweb.be>
2014-10-15 10:14:10 +0200

    Implemented editing of Maps (string and HTML text properties)

--- action.default.php
+++ action.default.php
@@ -4,25 +4,63 @@
 if (!isset($gCms)) exit;
 
 if (empty($params)) return;
-$params['page'] = ContentOperations::get_instance()->getContentObject()->Alias();
-$params['site'] = NetDesign::GetInstance()->GetSiteId();
-foreach($params as $key => $value) {
-    $skip = array('module', 'action');
-    if (in_array($key, $skip)) continue;
-    printf(' data-inline-editor-%s="%s"', $key, htmlentities($value));
-}
 
-// Set the value
-$table = $this->GetTable();
-$ret = $this->db->GetArray("SELECT * FROM `$table` WHERE `site_id` = ? AND `page_alias` = ? AND `name` = ?", array($params['site'], $params['page'], $params['name']));
-if (empty($ret) || empty($ret[0]['value'])) {
-    $value = null;
+// We always need these two
+$page = ContentOperations::get_instance()->getContentObject()->Alias();
+$site = NetDesign::GetInstance()->GetSiteId();
+
+// Add our HTML attributes
+if (array_key_exists('map', $params) && array_key_exists('property', $params) && array_key_exists('id', $params)) {
+    // We are editing a Map
+    $map = $params['map'];
+    $mid = $params['id'];
+    $prp = $params['property'];
+    /** @var MapBase $object */
+    $object = new $map($mid);
+    // Check if the property can be edited
+    $parser = $object->getParser();
+    $type = $parser->getConfig('type', $prp);
+    // Default attributes
+    $attrs = array('site' => $site, 'page' => $page, 'map' => $map, 'mid' => $mid, 'prp' => $prp);
+    // Generate a name
+    $attrs['name'] = sprintf('%s(%d).%s', $map, $mid, $prp);
+    switch($type) {
+        case 'string':
+            $attrs['type'] = 'string';
+            break;
+        case 'text':
+            $html = MapBase::castBool($parser->getConfig('text-html', $prp, false));
+            if (!$html) return;
+            $attrs['type'] = 'text';
+            break;
+        case 'thumbnail':
+            return;
+            break;
+    }
+    // Output the attributes
+    foreach($attrs as $key => $value) {
+        printf(' data-inline-editor-%s="%s"', $key, htmlentities($value));
+    }
 } else {
-    $ret = $ret[0];
-    $config = unserialize($ret['config']);
-    $value = $ret['value'];
-    if ($config['type'] == 'string') $value = htmlentities($value);
-    if (empty($value)) $value = null;
+    // Simple editing
+    $params['page'] = $page;
+    $params['site'] = $site;
+    foreach($params as $key => $value) {
+        $skip = array('module', 'action');
+        if (in_array($key, $skip)) continue;
+        printf(' data-inline-editor-%s="%s"', $key, htmlentities($value));
+    }
+    // Set the value
+    $table = $this->GetTable();
+    $ret = $this->db->GetArray("SELECT * FROM `$table` WHERE `site_id` = ? AND `page_alias` = ? AND `name` = ?", array($params['site'], $params['page'], $params['name']));
+    if (empty($ret) || empty($ret[0]['value'])) {
+        $value = null;
+    } else {
+        $ret = $ret[0];
+        $config = unserialize($ret['config']);
+        $value = $ret['value'];
+        if ($config['type'] == 'string') $value = htmlentities($value);
+        if (empty($value)) $value = null;
+    }
+    $this->smarty->assign('value', $value);
 }
-$this->smarty->assign('value', $value);
-
--- action.write.php
+++ action.write.php
@@ -6,8 +6,20 @@ if (!$this->CheckPermission('InlineEditor')) exit;
 
 $ret = true;
 if ($_POST['site'] != $this->GetSiteId()) {
+    // Shouldn't happen
     $ret = false;
+} elseif (array_key_exists('map', $_POST) && array_key_exists('mid', $_POST) && array_key_exists('prp', $_POST)) {
+    // Update a Map
+    $map = $_POST['map'];
+    $mid = $_POST['mid'];
+    $prp = $_POST['prp'];
+    $val = $_POST['value'];
+    /** @var MapBase $object */
+    $object = new $map($mid);
+    $object->$prp = $val;
+    $ret = $object->commit();
 } else {
+    // Simple editing
     $table = $this->GetTable();
     $value = $_POST['value'];
     unset($_POST['value']);
